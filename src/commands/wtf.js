b3ec2c63c2d411d7303e3ede826c148b689aec4c
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-09-09 11:42:49 -0300

    bugfix: algumas vezes o comando wtf falhava, agora não mais

--- src/commands/wtf.js
+++ src/commands/wtf.js
@@ -2,22 +2,28 @@ const ig = require('instagram-scraping');
 const { MessageAttachment } = require('discord.js');
 
 async function execute(message) {
-  const result = await ig.scrapeUserPage('stock.images.rar');
-  const imageNumber = calculateRandomNumber(result.total);
-  const { display_url, text } = result.medias[imageNumber];
+  let attemps = 0;
+  do {
+    var { display_url } = await getRandomMediaFromInstagramPage();
+    attemps++;
+  } while (!display_url && attemps < 3);
 
   const attachment = new MessageAttachment(display_url);
-  message.channel.send(`${text}`, attachment);
+  message.channel.send(attachment);
 }
 
-function calculateRandomNumber(max, min = 0) {
+async function getRandomMediaFromInstagramPage(page = 'stock.images.rar') {
+  const posts = await ig.scrapeUserPage(page);
+
+  const max = posts.total;
+  const min = 0;
   let randomNumber;
   do {
     randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
   } while (randomNumber === global.lastCalculatedNumber);
-
   global.lastCalculatedNumber = randomNumber;
-  return randomNumber;
+
+  return posts.medias[randomNumber];
 }
 
 module.exports = {
