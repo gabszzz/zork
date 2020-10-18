bf546cf283fb6592b408a9342e0425e23c8a45e3
Gabriel Teodoro <gabriel.skywalkerr@gmail.com>
2020-10-18 08:34:23 -0300

    add gaypercent command

---
+++ src/commands/gaypercent/main.js
@@ -0,0 +1,20 @@
+async function execute(message, args) {
+  if (!args[0]) {
+    message.reply('é necessário informar um usuário como parâmetro!');
+    return;
+  }
+
+  const percent = await getRandomGayPercent();
+  message.channel.send(`${args[0]} é ${percent}% gay.`);
+}
+
+async function getRandomGayPercent(min = 0, max = 100) {
+  const randomPercent = Math.floor(Math.random() * max + min);
+  return randomPercent;
+}
+
+module.exports = {
+  name: 'gaypercent',
+  description: 'Revela o quão gay uma pessoa é, em porcentagem.',
+  execute
+};
